42a64f04b49648921c5e648f21db018c7c71085c
abdullah2011-gif <60196750+abdullah2011-gif@users.noreply.github.com>
2020-10-29 11:54:54 +0500

    fixes

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
   // url: "http://localhost:8080/",
   url: "https://harpers-backend.herokuapp.com/",
-  url: "http://192.168.18.124:8080/",
+  // url: "http://192.168.18.124:8080/",
 };
 // "url":"https://safeline.azurewebsites.net/"
 //http://138.197.215.84:3000/
